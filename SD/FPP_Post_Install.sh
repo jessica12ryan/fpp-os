29291146815828b192927bf7491f06d6d07a3a43
jessica12ryan <jessica12ryan@gmail.com>
2026-05-15 20:33:26 -0300

    Install and enable systemd-timesyncd service

--- SD/FPP_Post_Install.sh
+++ SD/FPP_Post_Install.sh
@@ -1,4 +1,6 @@
 systemctl disable fpp-install.service
 systemctl daemon-reload
+apt-get install systemd-timesyncd -y
+systemctl enable --now systemd-timesyncd
 rm -rf /etc/ssh/ssh_host*key*
 shutdown -r now
