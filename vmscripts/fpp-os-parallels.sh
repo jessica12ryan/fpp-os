5701ffde59ec29f39471101bc491b8f40478be62
jessica12ryan <jessica12ryan@gmail.com>
2026-05-18 13:02:41 -0300

    Update parallels to bridged networking

--- vmscripts/fpp-os-parallels.sh
+++ vmscripts/fpp-os-parallels.sh
@@ -33,13 +33,17 @@ prlctl set "$VM_NAME" --device-del hdd0
 # 2. Configure Hardware Resources
 prlctl set "$VM_NAME" --memsize "$RAM_SIZE" --cpus 4
 
-# 3. Add your custom Hard Drive
+# 3. Configure Network to Bridged Mode
+# 'bridged' tells Parallels to bind to the default active network adapter (Wi-Fi or Ethernet)
+prlctl set "$VM_NAME" --device-set net0 --type bridged
+
+# 4. Add your custom Hard Drive
 prlctl set "$VM_NAME" --device-add hdd --type plain --size "$DISK_SIZE_MB"
 
-# 4. Attach Downloaded ISO
+# 5. Attach Downloaded ISO
 prlctl set "$VM_NAME" --device-set cdrom0 --image "$DOWNLOAD_PATH" --connect
 
-# 5. Boot the VM
+# 6. Boot the VM
 printf "\nSetup complete. Starting %s...\n" "$VM_NAME"
 prlctl start "$VM_NAME"
 printf "\nOpen Parallels to continue the VM installation...\n"
