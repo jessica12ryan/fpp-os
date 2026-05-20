5050c78f031b282a8a5ae61eea4cfb3eb44ef71c
jessica12ryan <jessica12ryan@gmail.com>
2026-05-20 00:30:42 -0300

    Remove fpp-install.service and FPP_Install.sh after installation

--- SD/FPP_Post_Install.sh
+++ SD/FPP_Post_Install.sh
@@ -1,6 +1,8 @@
 systemctl disable fpp-install.service
+rm -f /etc/systemd/system/fpp-install.service
 systemctl daemon-reload
 apt-get install systemd-timesyncd -y
 systemctl enable --now systemd-timesyncd
 rm -rf /etc/ssh/ssh_host*key*
+rm -f "$0"
 shutdown -r now
