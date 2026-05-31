459e74f646350ac22f693d1f9f1d2e1f9df03a37
jessica12ryan <jessica12ryan@gmail.com>
2026-05-31 03:16:30 -0300

    Update preload.js (syntax error)

--- flasher/src/preload.js
+++ flasher/src/preload.js
@@ -6,8 +6,8 @@ contextBridge.exposeInMainWorld('fpp', {
   listDrives:       ()              => ipcRenderer.invoke('list-drives'),
   downloadISO:      (url, isoName)  => ipcRenderer.invoke('download-iso', url, isoName),
   flashDrive:       (iso, device)   => ipcRenderer.invoke('flash-drive', iso, device),
-  onDownloadProgress: (cb)          => ipcRenderer.on('download-progress', (_e, pct) => cb(pct))
-  onFlashProgress:     (cb)              => ipcRenderer.on('flash-progress', (_e, d) => cb(d))
+  onDownloadProgress: (cb)          => ipcRenderer.on('download-progress', (_e, pct) => cb(pct)),
+  onFlashProgress:     (cb)              => ipcRenderer.on('flash-progress', (_e, d) => cb(d)),
   checkFlasherUpdate:       ()                => ipcRenderer.invoke('check-flasher-update'),
   downloadFlasherUpdate:    (url, name)       => ipcRenderer.invoke('download-flasher-update', url, name),
   openFlasherDmg:           (path)            => ipcRenderer.invoke('open-flasher-dmg', path),
